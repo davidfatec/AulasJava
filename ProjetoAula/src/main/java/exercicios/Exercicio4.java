25097489d3261a0002ba452f547197b346ad7aeb
David <89432341+davidfatec@users.noreply.github.com>
2022-02-21 23:57:00 -0300

    Update Exercicio4.java

--- ProjetoAula/src/main/java/exercicios/Exercicio4.java
+++ ProjetoAula/src/main/java/exercicios/Exercicio4.java
@@ -1,7 +1,8 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+Faça um algoritmo para ler dois números e realizar as operações aritméticas: 
+adição, subtração e multiplicação. Para cada operação o resultado deve ser 
+impresso no video.
+*/
 package exercicios;
 import java.util.Scanner;
 
@@ -12,5 +13,22 @@ import java.util.Scanner;
 public class Exercicio4 {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
+        //entrada de dados
+        System.out.println("Digite o primeiro numero: ");
+        int numero1 = scn.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int numero2 = scn.nextInt();
+        
+        //Operações
+        int soma = numero1 + numero2;
+        int subtracao = numero1 - numero2;
+        int multiplicacao = numero1 * numero2;
+
+        
+        //Saida dos dados processados
+        System.out.println("A soma de "+numero1+" e "+numero2+" é " + soma);
+        System.out.println("A subtação de "+numero1+" por "+numero2+" é " + subtracao);
+        System.out.println("A multiplicação de "+numero1+" por "+numero2+" é " + multiplicacao);
+     
     }
 }
