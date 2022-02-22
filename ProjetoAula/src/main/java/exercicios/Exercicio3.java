d18eefcf88d8d3b27ef1fa8c1d6a719e53273d0f
David <89432341+davidfatec@users.noreply.github.com>
2022-02-21 22:10:39 -0300

    Create Exercicio3.java

---
+++ ProjetoAula/src/main/java/exercicios/Exercicio3.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicios;
+import java.util.Scanner;
+/**
+ *
+ * @author david
+ */
+public class Exercicio3 {
+    public static void main(String[] args){
+       Scanner scn = new Scanner(System.in);
+       System.out.println("Nota 1: ");
+       float n1 = scn.nextFloat();
+       System.out.println("Nota 2: ");
+       float n2 = scn.nextFloat();
+       System.out.println("Nota 3: ");
+       float n3 = scn.nextFloat();
+       System.out.println("Nota 4: ");
+       float n4 = scn.nextFloat();
+       
+       float media = (((n1*1)+ (n2*2)+(n3*3)+(n4*4))/10);
+       
+       System.out.println("media ponderada: " + media);
+    }
+}
