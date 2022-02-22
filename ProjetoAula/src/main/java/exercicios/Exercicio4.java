02c82ff16b69070e2e4b6eab65f76d22cd0603ec
David <89432341+davidfatec@users.noreply.github.com>
2022-02-21 22:10:35 -0300

    Create Exercicio4.java

---
+++ ProjetoAula/src/main/java/exercicios/Exercicio4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicios;
+import java.util.Scanner;
+
+/**
+ *
+ * @author david
+ */
+public class Exercicio4 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+    }
+}
