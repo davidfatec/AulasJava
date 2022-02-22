d19f20ae2d8ada555f668f2a3b846507533abd7c
David <89432341+davidfatec@users.noreply.github.com>
2022-02-21 23:56:51 -0300

    Update Exercicio2.java

--- ProjetoAula/src/main/java/exercicios/Exercicio2.java
+++ ProjetoAula/src/main/java/exercicios/Exercicio2.java
@@ -1,7 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+Faça um algoritmo para ler um valor e fazer a troca dos valores digitados pelo 
+usuário. Após a troca, imprima os novos valores obtidos.
+*/
 package exercicios;
 import java.util.Scanner;
 
