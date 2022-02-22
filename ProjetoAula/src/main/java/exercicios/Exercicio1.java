eea5c48a553923b52296ecd3ee491db8847e03fb
David <89432341+davidfatec@users.noreply.github.com>
2022-02-21 23:56:47 -0300

    Create Exercicio1.java

---
+++ ProjetoAula/src/main/java/exercicios/Exercicio1.java
@@ -0,0 +1,18 @@
+/*
+Faça um algoritmo para ler um valor do usuário e imprimi-lo no video 
+*/
+package exercicios;
+import java.util.Scanner;
+
+/**
+ *
+ * @author david
+ */
+public class Exercicio1 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Digite um valor numerico: ");
+        int valor = scn.nextInt();
+        System.out.println("Valor digitado: " + valor);
+    }
+}
