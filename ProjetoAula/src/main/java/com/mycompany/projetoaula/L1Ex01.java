4af22ca3bec0643e7f7be50ec137f7113c9df184
David <89432341+davidfatec@users.noreply.github.com>
2022-03-07 20:12:40 -0300

    Create L1Ex01.java

---
+++ ProjetoAula/src/main/java/com/mycompany/projetoaula/L1Ex01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetoaula;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author david
+ */
+public class L1Ex01 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Digite um valor numerico: ");
+        int valor = scn.nextInt();
+        System.out.println("Valor digitado: " + valor);      
+    }
+}
