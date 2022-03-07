82fe8ec75e1ec4cd447ceb34a8c04af264ef50c1
David <89432341+davidfatec@users.noreply.github.com>
2022-03-07 20:13:02 -0300

    Create L1Ex05.java

---
+++ ProjetoAula/src/main/java/com/mycompany/projetoaula/L1Ex05.java
@@ -0,0 +1,24 @@
+
+package com.mycompany.projetoaula;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author david
+ */
+public class L1Ex05 {
+    public static void main(String[] args){
+       Scanner scn = new Scanner(System.in);
+       System.out.println("Multiplos de 5.");
+       System.out.println("Digite o número inicial: ");
+       int numeroInicio = scn.nextInt();
+       System.out.println("Digite o número limite: ");
+       int numeroFinal = scn.nextInt();
+       System.out.println("Impressão dos multiplos de ");
+       for(int i=numeroInicio;i<=numeroFinal;i++){
+	    if(i % 5 == 0)
+		System.out.println(i);			
+       }
+    }
+}
